5ea4ad47122be57dd1ee8176dc0476b1711521b0
TonySpoon <jaxsonbanser@gmail.com>
2026-06-10 12:02:25 -0500

    Added game over functionality by disabling the button when you run out of guesses

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { useState } from 'react';
-import './App.css'
 import toast, { Toaster } from 'react-hot-toast';
+
+import './App.css'
 
 function App() {
 
   const[blur, setBlur] = useState(50)
   const[guess, setGuess] = useState('')
 
-  const blurReduction = 10
+  const BLURREDUCTION = 10
 
-  const toastFadeDuration = 2000
+  const FADEDURATION = 2000
 
   const handleGuess = () => { //Handles logic for inputted guesses
     toast.dismissAll() //Dismisses any existing toasts before showing a new one
@@ -18,15 +19,17 @@ function App() {
       return
     }
 
-    if (guess.toLowerCase() === 'feedbacker') { //If user guesses correctly, show a success message and end the game
+    if (guess.trim().toLowerCase() === 'feedbacker') { //If user guesses correctly, show a success message and end the game
       toast.success('Correct! The album is "Feedbacker" by Boris.')
       setBlur(0)
     } else { //If the guess is incorrect, handles game logic
-      if (blur - blurReduction < 0) { //If the user has run out of attempts, show a game over message and end the game
+      if (blur - BLURREDUCTION < 0) { //If the user has run out of attempts, show a game over message and end the game
         toast.error('Game over! You have run out of attempts.')
+        setBlur(-1)
+        
       } else { //If the user still has attempts left, show an error message and reduce the blur
         toast.error('Incorrect guess. Try again!')
-        setBlur(blur - blurReduction)
+        setBlur(blur - BLURREDUCTION)
       }
     }
     setGuess('')
@@ -38,7 +41,7 @@ function App() {
 
       <Toaster 
         position="bottom-center"
-        toastOptions={{ duration: toastFadeDuration }}
+        toastOptions={{ duration: FADEDURATION }}
        />
 
       <div className="album-container">
@@ -55,10 +58,12 @@ function App() {
           value={guess} 
           onChange={(e) => setGuess(e.target.value)} 
           onKeyDown={(e) => e.key === 'Enter' && handleGuess()}
+          disabled={blur<0}
           type="text" placeholder="Enter your guess..." 
           className="guess-input" />
 
         <button 
+          disabled={blur<0}
           className="guess-button" 
           onClick={handleGuess}>Guess
         </button>
