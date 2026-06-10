c935c55d155eb358117645b69e6cea26fbe4d0e0
TonySpoon <jaxsonbanser@gmail.com>
2026-06-10 11:23:15 -0500

    Added Toast popups for incorrect and correct guesses

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
 import { useState } from 'react';
 import './App.css'
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
 
   const[blur, setBlur] = useState(50)
   const[guess, setGuess] = useState('')
 
-  const handleGuess = () => {
-    if (guess.trim().toLowerCase() === '') {
-      
+  const blurReduction = 10
+
+  const toastFadeDuration = 2000
+
+  const handleGuess = () => { //Handles logic for inputted guesses
+    toast.dismissAll() //Dismisses any existing toasts before showing a new one
+    if (guess.trim().toLowerCase() === '') { //If the user enters a blank guess, print an error message but do not reduce the blur
+      toast.error('Please enter a guess before submitting.')
       return
     }
 
-    if (guess.toLowerCase() === 'feedbacker') {
-      alert('Correct! The album is "Feedbacker" by Boris.')
-    } else {
-      setBlur(blur - 10)
+    if (guess.toLowerCase() === 'feedbacker') { //If user guesses correctly, show a success message and end the game
+      toast.success('Correct! The album is "Feedbacker" by Boris.')
+      setBlur(0)
+    } else { //If the guess is incorrect, handles game logic
+      if (blur - blurReduction < 0) { //If the user has run out of attempts, show a game over message and end the game
+        toast.error('Game over! You have run out of attempts.')
+      } else { //If the user still has attempts left, show an error message and reduce the blur
+        toast.error('Incorrect guess. Try again!')
+        setBlur(blur - blurReduction)
+      }
     }
     setGuess('')
   }
@@ -23,6 +35,11 @@ function App() {
   return (
     <div> 
       <h1>Coverdle</h1>
+
+      <Toaster 
+        position="bottom-center"
+        toastOptions={{ duration: toastFadeDuration }}
+       />
 
       <div className="album-container">
         <img 
