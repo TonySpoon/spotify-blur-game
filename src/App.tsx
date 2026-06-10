7a129c9d24477d8b7e09aa2b736803e77dfa2954
TonySpoon <jaxsonbanser@gmail.com>
2026-06-10 10:41:39 -0500

    Added function to handle guesses & 'enter' key functionality for submitting a guess

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+
+  const[blur, setBlur] = useState(50)
+  const[guess, setGuess] = useState('')
+
+  const handleGuess = () => {
+    if (guess.trim().toLowerCase() === '') {
+      
+      return
+    }
+
+    if (guess.toLowerCase() === 'feedbacker') {
+      alert('Correct! The album is "Feedbacker" by Boris.')
+    } else {
+      setBlur(blur - 10)
+    }
+    setGuess('')
+  }
+
   return (
     <div> 
       <h1>Coverdle</h1>
@@ -10,12 +29,22 @@ function App() {
           src="/FeedbackerImg.jpg"
           alt="Album Cover"
           className="album-cover"
+          style={{ filter: `blur(${blur}px)` }}
         />  
       </div>
 
       <div className="guess-container">
-        <input type="text" placeholder="Enter your guess..." className="guess-input" />
-        <button className="guess-button">Guess</button>
+        <input 
+          value={guess} 
+          onChange={(e) => setGuess(e.target.value)} 
+          onKeyDown={(e) => e.key === 'Enter' && handleGuess()}
+          type="text" placeholder="Enter your guess..." 
+          className="guess-input" />
+
+        <button 
+          className="guess-button" 
+          onClick={handleGuess}>Guess
+        </button>
       </div>
 
     </div>
